2562fdb034d2489dcc993e33496b85bd7cc0a0f7
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-06-17 11:53:00 +1000

    Use routing

--- src/sections/Nav/Nav.tsx
+++ src/sections/Nav/Nav.tsx
@@ -5,17 +5,19 @@ function Nav() {
   return (
     <nav className={style.container}>
       <div>
-        <h1>AIcons</h1>
+        <h1>
+          <a href="./">AIcons</a>
+        </h1>
       </div>
       <ul className={style.navItems}>
         <li>
-          <a href="#">Generate</a>
+          <a href="./generate">Generate</a>
         </li>
         <li>
-          <a href="#">Option</a>
+          <a href="#">Icons</a>
         </li>
         <li>
-          <a href="#">Option</a>
+          <a href="#">Sign In</a>
         </li>
       </ul>
     </nav>
