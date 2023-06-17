e25ce362c6351f868ba3c90e86d5cd343fe8b372
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-06-17 11:52:36 +1000

    Add Generate section

---
+++ src/sections/Generate/Generate.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import style from "./Generate.module.css";
+
+interface TypeProps {}
+
+const Generate: FC<TypeProps> = (props) => {
+  return (
+    <section className={style.container}>
+      <form></form>
+    </section>
+  );
+};
+
+export default Generate;
