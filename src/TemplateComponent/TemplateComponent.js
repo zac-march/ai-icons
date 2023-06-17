b55b9d85b615d546217f541021f9841b528b9321
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-06-17 10:02:02 +1000

    Initialise react app

---
+++ src/TemplateComponent/TemplateComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./TemplateComponent.module.css";
+
+function TemmplateComponent() {
+  return <div className={style.container}></div>;
+}
+
+export default TemmplateComponent;
