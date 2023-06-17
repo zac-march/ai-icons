b2ac860a0667225ff7b69a545a49cd75ea5b38ca
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-06-17 11:48:10 +1000

    Implement section router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Nav from "./sections/Nav/Nav";
 import Hero from "./sections/Hero/Hero";
+import Generate from "./sections/Generate/Generate";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <main>
-        <Hero />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <main>
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/generate" element={<Generate />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
