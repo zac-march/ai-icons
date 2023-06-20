e166e6f675ac9e9fbbb1f84ee5d2c949c0fd046c
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-06-20 17:15:12 +1000

    Add icons for design options

---
+++ src/data/iconDesigns.js
@@ -0,0 +1,45 @@
+import icon3DDetailed from "../assets/premade-icons/rocket-3ddetailed.png";
+import iconAnimation from "../assets/premade-icons/rocket-animation.png";
+import iconCartoon from "../assets/premade-icons/rocket-cartoon.png";
+import iconDigitalArt from "../assets/premade-icons/rocket-digitalart.png";
+import iconDoodle from "../assets/premade-icons/rocket-doodle.png";
+import iconFlat from "../assets/premade-icons/rocket-flat.png";
+import iconShiny from "../assets/premade-icons/rocket-shiny.png";
+import icon3DSimple from "../assets/premade-icons/rocketship-3dsimple.png";
+
+const designOptions = [
+  {
+    name: "Flat",
+    img: iconFlat,
+  },
+  {
+    name: "Simple 3D",
+    img: icon3DSimple,
+  },
+  {
+    name: "Digital Art",
+    img: iconDigitalArt,
+  },
+  {
+    name: "Detailed 3D",
+    img: icon3DDetailed,
+  },
+  {
+    name: "Shiny",
+    img: iconShiny,
+  },
+  {
+    name: "Cartoon",
+    img: iconCartoon,
+  },
+  {
+    name: "Doodle",
+    img: iconDoodle,
+  },
+  {
+    name: "Animation",
+    img: iconAnimation,
+  },
+];
+
+export default designOptions;
