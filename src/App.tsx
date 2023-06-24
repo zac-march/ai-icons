0dbf76e2ac6aeccc58afd02969a6540210e60f31
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-06-24 14:05:04 +1000

    Rename component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Nav from "./sections/Nav/Nav";
-import Hero from "./sections/Hero/Hero";
+import Landing from "./sections/Landing/Landing";
 import Generate from "./sections/Generate/Generate";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Nav />
         <main>
           <Routes>
-            <Route path="/" element={<Hero />} />
+            <Route path="/" element={<Landing />} />
             <Route path="/generate" element={<Generate />} />
           </Routes>
         </main>
