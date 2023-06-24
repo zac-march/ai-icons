db16bddcc0a0d79ab9eae6b7907f348e3d99af7e
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-06-24 14:02:42 +1000

    Add mobile nav

--- src/sections/Nav/Nav.tsx
+++ src/sections/Nav/Nav.tsx
@@ -3,24 +3,30 @@ import style from "./Nav.module.css";
 
 function Nav() {
   return (
-    <nav className={style.container}>
-      <div>
-        <h1>
+    <header className={style.container}>
+      <nav className={style.nav}>
+        <label className={style.logo}>
           <a href="./">AIcons</a>
-        </h1>
-      </div>
-      <ul className={style.navItems}>
-        <li>
-          <a href="./generate">Generate</a>
-        </li>
-        <li>
-          <a href="#">Icons</a>
-        </li>
-        <li>
-          <a href="#">Sign In</a>
-        </li>
-      </ul>
-    </nav>
+        </label>
+        <input type="checkbox" id={style["menuToggle"]} />
+        <label htmlFor={style["menuToggle"]} className={style.menuBtn}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+        <ul>
+          <li>
+            <a href="./generate">Generate</a>
+          </li>
+          <li>
+            <a href="#">Icons</a>
+          </li>
+          <li>
+            <a href="#">Sign In</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
